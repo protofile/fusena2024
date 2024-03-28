85a1f1189b8553421f3574aad952bcf1ea3d5489
yudantoardi <yudantoardi@gmail.com>
2024-03-28 12:59:51 +0700

    updates

--- asset/js/main.js
+++ asset/js/main.js
@@ -5,4 +5,19 @@ $(document).ready(function(){
         $(this).parent().addClass("active");
     });
 
+    $(window).scroll(function(){
+        if ($(window).scrollTop() >= 100) {
+            $('header').addClass('sticky');
+        }
+        else {
+            $('header').removeClass('sticky');
+        }
+    });
+
+    $(".burger-bt").click(function(bbt){
+        bbt.preventDefault();
+        $($(this).attr("href")).toggleClass("show");
+        $(this).toggleClass("clicked");
+    });
+
 });
