4873c54c3e4dd0673490332536ffb5b2d49066ac
yudantoardi <yudantoardi@gmail.com>
2024-06-26 20:39:00 +0700

    banner video

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,23 +1,28 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  $(".fs-acc-head").click(function () {
+    $(".fs-acc-item").removeClass("active");
+    $(this).parent().addClass("active");
+  });
 
-    $(".fs-acc-head").click(function(){
-        $(".fs-acc-item").removeClass("active");
-        $(this).parent().addClass("active");
-    });
+  $(window).scroll(function () {
+    if ($(window).scrollTop() >= 100) {
+      $("header").addClass("sticky");
+    } else {
+      $("header").removeClass("sticky");
+    }
+  });
 
-    $(window).scroll(function(){
-        if ($(window).scrollTop() >= 100) {
-            $('header').addClass('sticky');
-        }
-        else {
-            $('header').removeClass('sticky');
-        }
-    });
+  $(".burger-bt").click(function (bbt) {
+    bbt.preventDefault();
+    $($(this).attr("href")).toggleClass("show");
+    $(this).toggleClass("clicked");
+  });
 
-    $(".burger-bt").click(function(bbt){
-        bbt.preventDefault();
-        $($(this).attr("href")).toggleClass("show");
-        $(this).toggleClass("clicked");
-    });
-
+  $(".accordion-content").hide();
+  $(".accordion-header").click(function () {
+    $(".accordion-content").hide();
+    $(".accordion-header").removeClass("active");
+    $(this).next(".accordion-content").slideToggle();
+    $(this).addClass("active");
+  });
 });
